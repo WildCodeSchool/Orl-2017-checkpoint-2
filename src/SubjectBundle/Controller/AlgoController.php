217626aa917cd931a3f9e685cb7708b9f85ab348
julien <julienmartin.opto@gmail.com>
2017-04-14 09:45:14 +0200

    Algo finie

--- src/SubjectBundle/Controller/AlgoController.php
+++ src/SubjectBundle/Controller/AlgoController.php
@@ -12,6 +12,23 @@ class AlgoController extends Controller
     //
     public function strlenOrder($string, $order)
     {
+        $words = explode(' ', $string);
 
+        foreach ($words as $word) {
+            $wordsLength[$word] = strlen($word);
+        }
+
+        if ('ASC' == $order) {
+            asort($wordsLength);
+        } elseif ('DESC' == $order) {
+            arsort($wordsLength);
+        } else {
+            return false;
+        }
+
+        $wordsLength = array_flip($wordsLength);
+        $result = implode(' ', $wordsLength);
+
+        return $result;
     }
 }
